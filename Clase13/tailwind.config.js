abdc5d162031269012673a4be5814cd9b95e9670
Santiago Harkes <santiagoharkes@gmail.com>
2020-11-08 14:51:13 -0300

    Mas clases

---
+++ Clase13/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontSize: {
+        fotolog: "10rem",
+      },
+      colors: {
+        colorPrincipal: {
+          100: "#0353A4",
+          200: "#061A40",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
